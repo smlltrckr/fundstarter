b2c74069ff0a4a5f20a8fb835decf95b920532e6
Ubuntu <ubuntu@ip-172-31-36-48.us-west-2.compute.internal>
2016-03-15 07:46:10 +0000

    Part 2

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ var requestListener = function (request, response) {
         if (errStat) {
             return console.log(errStat);   
         }
-	console.log(stats.size);
-        var buffer = new buffer(stats.size);
-        var offset = 0,
-            length = buffer.length,
-            position = 0;
-           	
+	           	
 	fs.open(filename, 'r', function (err, fd) {
             if (err) {
                 return console.log(err);
             }
-            fs.read(fd, buffer, offset, length, position, function (errRead, bytesRead) { 
+	    console.log(stats.size);
+            var buffer = new buffer(2489);
+            var offset = 0,
+            length = buffer.length,
+            position = 0;
+            
+	    fs.read(fd, buffer, offset, length, position, function (errRead, bytesRead) { 
                 if (errRead) {
                     return console.log(errRead);
                 }
