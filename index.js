adf88bcda40170cd399f860535614fb6f2c8ceae
Ubuntu <ubuntu@ip-172-31-36-48.us-west-2.compute.internal>
2016-03-15 20:37:55 +0000

    Part 2

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+/*
+    Part 1a http server using readFileSync
+    under commit with message "make it better"
+*/
+/*
+var http = require('http'),
+    fs = require('fs');
+
+var requestListener = function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(fs.readFileSync('index.html'));
+}
+
+var server = http.createServer(requestListener);
+server.listen(process.env.PORT || 8080);
+*/
+/*
+    Part 1b http server using readFile
+    under commit with "part 1b fs.readFile"
+*/
+/*
+var http = require('http'),
+    fs = require('fs');
+
+var requestListener = function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        response.write(data);
+        response.end();
+    });
+}
+
+var server = http.createServer(requestListener);
+server.listen(process.env.PORT || 8080);
+*/
+/*
+    Part 2 
+*/
+
 var http = require('http'),
     fs = require('fs'),
     filename = 'index.html';
@@ -8,25 +50,22 @@ var requestListener = function (request, response) {
         if (errStat) {
             return console.log(errStat);   
         }
-	           	
-	fs.open(filename, 'r', function (err, fd) {
+        console.log(stats.size);
+        var buffer = new Buffer(stats.size);
+        var offset = 0,
+            length = buffer.length;
+
+        fs.open(filename, 'r', function (err, fd) {
             if (err) {
                 return console.log(err);
             }
-	    console.log(stats.size);
-            var buffer = new buffer(stats.size);
-            var offset = 0,
-            length = buffer.length,
-            position = 0;
-            
-	    fs.read(fd, buffer, offset, length, position, function (errRead, bytesRead, buff) { 
+            fs.read(fd, buffer, offset, length, null, function (errRead, bytesRead, buff) {
                 if (errRead) {
                     return console.log(errRead);
                 }
                 response.write(buff.toString("utf8", 0, bytesRead));
                 console.log(buff.toString("utf8", 0, bytesRead));
                 response.end();
-		fs.close(fd);
                 });
             });
     });
