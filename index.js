129d25bd0e90cfd46517658d94d273712639d692
Ubuntu <ubuntu@ip-172-31-36-48.us-west-2.compute.internal>
2016-03-15 04:26:40 +0000

    make it better

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-var express = require('express')
-var app = express()
+var http = require('http'),
+    fs = require('fs');
 
-/* serves all the static files*/
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
+var requestListener = function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(fs.readFileSync('/index.html'));
+    res.end();
+}
 
-/*serves main page*/
-app.get('/', function(request, response) {
-response.sendfile('index.html') 
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var server = http.createServer(requestListener);
+server.listen(8080);
